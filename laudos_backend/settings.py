0b9dab461e8aad71c19249304ca72fe886abfbe4
rafaelpsmed <rafael.univasf@gmail.com>
2025-11-01 18:05:49 -0300

    atualizações e configuração do django admin

--- laudos_backend/settings.py
+++ laudos_backend/settings.py
@@ -138,10 +138,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'laudos_backend.wsgi.application'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 # =============================================================================
 # CONFIGURAÇÃO SEGURO DO BANCO DE DADOS
 # =============================================================================
@@ -220,7 +216,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
